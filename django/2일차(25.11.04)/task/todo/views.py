d90f43ebade89f83947ba919cb3f0ba0282c1269
choiguen <daindj0@gmail.com>
2025-11-16 17:59:04 +0900

    6일차 과제 완료

--- django/2일차(25.11.04)/task/todo/views.py
+++ django/2일차(25.11.04)/task/todo/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Q
-
-from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
@@ -13,26 +10,28 @@ from todo.models import Todo
 
 @login_required()
 def todo_list(request):
-    todos = Todo.objects.filter(user=request.user).order_by('created_at')
+    todo_list = Todo.objects.filter(user=request.user).order_by('created_at')
     q = request.GET.get('q')
     if q:
-        todos = todos.filter(Q(title__icontains=q) | Q(description__icontains=q))
-    paginator = Paginator(todos, 10)
+        todo_list = todo_list.filter(Q(title__icontains=q) | Q(description__icontains=q))
+    paginator = Paginator(todo_list, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
-        'page_obj': page_obj,
+        'page_obj': page_obj
     }
-    return render(request, "todo/todo.html", context)
+    print(page_obj)
+    return render(request, 'todo/todo_list.html', context)
 
 
 @login_required()
 def todo_info(request, todo_id):
     todo = get_object_or_404(Todo, id=todo_id)
     context = {
-        'todo':todo.__dict__,
+        'todo': todo.__dict__  # items 메서드를 사용하기 위해 딕셔너리 형태로 context를 넘겨줍니다.
     }
-    return render(request, "todo/todo_info.html", context)
+    return render(request, 'todo/todo_info.html', context)
+
 
 @login_required()
 def todo_create(request):
@@ -41,11 +40,12 @@ def todo_create(request):
         todo = form.save(commit=False)
         todo.user = request.user
         todo.save()
-        return redirect(reverse('todo_info', kwargs={'pk':todo.pk}))
+        return redirect(reverse('todo_info', kwargs={'todo_id': todo.pk}))
     context = {
-        'form': form,
+        'form': form
     }
-    return render(request, "todo/todo_create.html", context)
+    return render(request, 'todo/todo_create.html', context)
+
 
 @login_required()
 def todo_update(request, todo_id):
@@ -53,11 +53,12 @@ def todo_update(request, todo_id):
     form = TodoUpdateForm(request.POST or None, instance=todo)
     if form.is_valid():
         form.save()
-        return redirect(reverse('todo_info', kwargs={'todo_id': todo_id}))
+        return redirect(reverse('todo_info', kwargs={'todo_id': todo.pk}))
     context = {
-        'form': form,
+        'form': form
     }
     return render(request, 'todo/todo_update.html', context)
+
 
 @login_required()
 def todo_delete(request, todo_id):
--- django/2일차(25.11.04)/task/users/views.py
+++ django/2일차(25.11.04)/task/users/views.py
@@ -1,20 +1,18 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as django_login
+
 
 def sign_up(request):
     form = UserCreationForm(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect(settings.LOGIN_URL)
-    context = {
-        'form':form
-    }
+
+    context = {'form': form}
     return render(request, 'registration/signup.html', context)
+
 
 def login(request):
     form = AuthenticationForm(request, data=request.POST or None)
@@ -22,8 +20,5 @@ def login(request):
         django_login(request, form.get_user())
         return redirect(settings.LOGIN_REDIRECT_URL)
 
-    context = {
-        'form':form
-    }
-    return render(request, "registration/login.html", context)
-
+    context = {'form': form}
+    return render(request, 'registration/login.html', context)
